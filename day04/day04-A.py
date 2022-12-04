a0f33fad5f3ec261b809f838121c329974a58b9a
Peter Bui <pbui@bx612.space>
2022-12-04 08:22:39 -0500

    Day 04

---
+++ day04/day04-A.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Day 04: A
+
+import re
+import sys
+
+# Constants
+
+PAIRS_RX = r'(\d+)-(\d+),(\d+)-(\d+)'
+
+# Functions
+
+def read_pairs(stream=sys.stdin):
+    return [
+        (set(range(As, Ae + 1)), set(range(Bs, Be + 1)))
+        for As, Ae, Bs, Be in map(lambda l: map(int, re.findall(PAIRS_RX, l)[0]), stream)
+    ]
+
+# Main Execution
+
+def main():
+    pairs     = read_pairs()
+    contained = sum(
+        1 for p1, p2 in pairs if (p1 & p2) in (p1, p2)
+    )
+    print(f'Day 4-A: {contained}')
+
+if __name__ == '__main__':
+    main()
