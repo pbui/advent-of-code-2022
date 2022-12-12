e2725f8ec7310843c6cbd5e189555a6d57be5127
Peter Bui <pbui@bx612.space>
2022-12-12 09:50:07 -0500

    Day 12: A

---
+++ day12/day12-A.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import collections
+import heapq
+import itertools
+import sys
+
+# Constants
+
+DIRECTIONS = (
+    (-1,  0),   # Up
+    ( 1,  0),   # Down
+    ( 0, -1),   # Left
+    ( 0,  1),   # Right
+)
+
+# Functions
+
+def read_grid(stream=sys.stdin):
+    grid = ['Z' + line.strip() + 'Z' for line in stream]
+    grid.insert(0, 'Z'*len(grid[0]))
+    grid.append('Z'*len(grid[0]))
+    return grid
+
+def build_graph(grid):
+    graph = collections.defaultdict(set)
+    rows  = list(range(1, len(grid) - 1))
+    cols  = list(range(1, len(grid[0]) - 1))
+    start = None
+    end   = None
+
+    for r, c in itertools.product(rows, cols):
+        v = (r - 1)*len(cols) + (c - 1)
+        h = grid[r][c]
+
+        if h == 'S': start = v
+        if h == 'E': end   = v
+
+        for dr, dc in DIRECTIONS:
+            n_r = r + dr
+            n_c = c + dc
+            n_v = (n_r - 1) * len(cols) + (n_c - 1)
+            n_h = grid[n_r][n_c]
+
+            if n_h == 'Z':
+                continue
+
+            if n_h == 'E' and h != 'z':
+                continue
+
+            if h == 'S' or abs(ord(h) - ord(n_h)) <= 1 or ord(h) > ord(n_h):
+                graph[v  ].add(n_v)
+
+    return graph, start, end
+
+def walk_graph(graph, start, end):
+    frontier = [(0, start)]
+    visited  = set()
+
+    while frontier:
+        distance, vertex = heapq.heappop(frontier)
+
+        if vertex in visited:
+            continue
+
+        if vertex == end:
+            return distance
+
+        visited.add(vertex)
+
+        for neighbor in graph[vertex]:
+            heapq.heappush(frontier, (distance + 1, neighbor))
+
+# Main Execution
+
+def main():
+    grid              = read_grid()
+    graph, start, end = build_graph(grid)
+    steps             = walk_graph(graph, start, end)
+
+    print(f'Day 12-A: {steps}')
+
+if __name__ == '__main__':
+    main()
